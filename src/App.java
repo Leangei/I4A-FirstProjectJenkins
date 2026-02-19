b8b5c538cc73b9dd91ba8ecd7ffafca40fd9528b
Leangei <leangeimeng1602@gmail.com>
2026-02-19 14:45:20 +0700

    correct-error

--- src/App.java
+++ src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.printf("Hello, World %d!");
+        System.out.println("Hello, World!");
     }
 }
